bc61a53928bc1374fa61c63bf0c8604e92be4e41
Berat Nakip <g141210084@sakarya.edu.tr>
2018-01-24 17:06:06 +0300

    ilk commit

---
+++ app/User.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App;
+
+use App\Post;
+use App\SocialAccount;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
+
+class User extends Authenticatable
+{
+    use Notifiable, HasApiTokens;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    public function socialAccounts(){
+        return $this->hasMany(SocialAccount::class);
+    }
+
+    public function posts(){
+        return $this->hasMany(Post::class);
+    }
+}
